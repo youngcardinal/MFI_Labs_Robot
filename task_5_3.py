243001878d2df7d54d13afcd9a5b774eb6c991f9
Farid <siberia.on@gmail.com>
2020-10-22 17:14:40 +0500

    Initial commit

---
+++ task_5_3.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+from pyrob.api import *
+
+
+@task
+def task_5_2():
+    def check_wall():   # поверяем есть ли стена снизу, иначе "стоп"
+        while wall_is_beneath() == True:
+            move_right()
+    move_right()
+    check_wall()
+
+
+if __name__ == '__main__':
+    run_tasks()
