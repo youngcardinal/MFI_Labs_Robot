578765143aa2100fac3e4bfc578ddc5edc1dba5e
Farid <siberia.on@gmail.com>
2020-10-24 14:27:45 +0500

    Переписал цикл через not

--- task_3_3.py
+++ task_3_3.py
@@ -5,11 +5,11 @@ from pyrob.api import *
 
 @task
 def task_3_3():
-    if wall_is_above() == False:            # проверяем наличие стены сверху
+    if not wall_is_above():            # проверяем наличие стены сверху
         move_up()
-    elif wall_is_on_the_right() == False:   # проверяем наличие стены справа
+    elif not wall_is_on_the_right():   # проверяем наличие стены справа
         move_right()
-    elif wall_is_beneath() == False:        # проверяем наличие стены снизу
+    elif not wall_is_beneath():        # проверяем наличие стены снизу
         move_down()
     else:
         move_left()                         # иначе двигаемся влево
